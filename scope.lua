3ef85796cc6c50f5115d0cc7bea1c2ffd6d53259
keenan <keemcd@gmail.com>
2020-06-08 21:56:33 +0700

    added rate control, offset adjust

--- scope.lua
+++ scope.lua
@@ -1,20 +1,18 @@
 -- scope for crow
-
--- rising: crow study 2
--- input modes
 --
--- input 1 is stream/query
--- input 2 is change
---
--- K2 query
--- K3 toggle mode
+--key 2: toggle xaxis
+--enc 2: adjust rate
+--enc 3: adjust xaxis offset
 
 local volts = 0
 local state = 0
 local history = {}
+local rate = 50
+local xaxis = false
+local offset = 24
 
 function init()
-  crow.input[1].mode("stream")
+  crow.input[1].mode("stream", 1/rate)
   crow.input[1].stream = stream
   screen.level(15)
   screen.aa(0)
@@ -31,7 +29,7 @@ end
 
 function stream(v)
   volts = v
-  history[0] = v
+  history[0] = round(v*-2)
   for i=127, 1, -1 do
     history[i] = history[i-1]
   end
@@ -41,13 +39,30 @@ end
 
 function redraw()
   screen.clear()
-  screen.move(6,60)
+  screen.move(2,60)
   screen.text("volts: "..string.format("%.3f",volts))
+  screen.move(60, 60)
+  --screen.text("offset: "..string.format(offset))
+  screen.text("rate: "..string.format("%.2f",rate).."px/s")
+  screen.line_width(1)
+  --screen.pixel(40,40)
   for i = 127, 0, -1 do
-    screen.pixel(i, history[i]*-4 + 30)
+    --screen.pixel(i, history[i])
+    screen.move(i, history[i] + offset)
+    screen.line_rel(0, -1)
+    screen.stroke()
+  end
+  if xaxis then
+    screen.move(0, offset)
+    screen.line_rel(127,0)
     screen.stroke()
   end
   screen.update()
+end
+
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(math.floor(num * mult + 0.5) / mult)
 end
 
 function getLength(t)
@@ -58,5 +73,19 @@ function getLength(t)
   return count
 end
 
-function key(n,z)
+function enc(n,z)
+  if n==2 then
+    rate = util.clamp(rate + z, 1, 100)
+    crow.input[1].mode("stream", 1/rate)
+  elseif n==3 then
+    offset = util.clamp(offset - z, 0, 60)
+  end
 end 
+
+function key(n, z)
+  if n==2 and z==1 and xaxis then
+    xaxis = false
+  elseif n==2 and z==1 and not axis then
+    xaxis = true
+  end
+end
--- untitled.lua
+++
@@ -1,2 +0,0 @@
--- scope for crow
-
