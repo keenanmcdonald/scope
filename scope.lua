dd1cadb56ad50a904c1427ceb348f8abe8dc6ead
keenan <keemcd@gmail.com>
2020-06-09 23:08:11 +0700

    added display modes, xaxis display, need to fix xaxis specificity

--- scope.lua
+++ scope.lua
@@ -2,24 +2,26 @@
 --
 --key 2: toggle xaxis
 --key 3: toggle input 2 display
---enc 2: adjust rate
+--enc 2: adjust rate (hold key 1 to finetune)
 --enc 3: adjust xaxis offset
 
 local volts1 = 0
 local volts2 = 0
 local state = 0
-history1 = {}
-history2 = {}
-local rate = 50
+local history1 = {}
+local history2 = {}
+local rate = .5
 local xaxis = false
 local yrange = 2
 local offset = 24
-display2 = false
+local display2 = false
+local displayMode = 1
+local enc3Mode = 'yrange'
 
 function init()
-  crow.input[1].mode("stream", 1/rate)
+  crow.input[1].mode("stream", 1/(rate*127))
   crow.input[1].stream = stream1
-  crow.input[2].mode("stream", 1/rate)
+  crow.input[2].mode("stream", 1/(rate*127))
   crow.input[2].stream = stream2
   screen.level(15)
   screen.aa(0)
@@ -60,19 +62,34 @@ end
 
 function redraw()
   screen.clear()
-  screen.move(2,60)
-  screen.text("volts: "..string.format("%.3f",volts1))
-  screen.move(60, 60)
-  --screen.text("offset: "..string.format(offset))
-  screen.text("rate: "..string.format("%.2f",rate).."px/s")
+  if displayMode == 1 then
+    screen.move(2,60)
+    screen.text("volts(1): "..string.format("%.3f",volts1))
+    screen.move(64, 60)
+    screen.text("volts(2): "..string.format("%.3f",volts2))
+  elseif displayMode == 2 then
+    screen.move(2,60)
+    screen.text("rate: "..string.format(rate).." hz")
+  elseif displayMode == 3 then
+    screen.move(0, 56)
+    screen.line_rel(127, 0)
+    xaxisRange = math.floor(1/rate)
+    xaxisSpec = 2
+    for i=1, xaxisRange*xaxisSpec do
+      screen.move(round((127*rate*i)/xaxisSpec), 56)
+      screen.line_rel(0,2)
+      screen.move(round((127*rate*i)/xaxisSpec)-2, 64)
+      screen.font_size(8)
+      screen.text(string.format(i/xaxisSpec))
+    end
+  end
   screen.line_width(1)
-  --screen.pixel(40,40)
   for i = 127, 0, -1 do
-    screen.move(i, round(history1[i]*yrange*-1) + offset)
+    screen.move(127-i, round(history1[i]*yrange*-1) + offset)
     screen.line_rel(0, -1)
     screen.stroke()
     if display2 then
-      screen.move(i, round(history2[i]*yrange*-1) + offset)
+      screen.move(127-i, round(history2[i]*yrange*-1) + offset)
       screen.line_rel(0, -1)
       screen.stroke()
     end
@@ -100,23 +117,36 @@ end
 
 function enc(n,z)
   if n==1 then
-    yrange = util.clamp(yrange + z*.5, 1, 8)
+    displayMode = util.clamp(displayMode + z, 1, 3)
   elseif n==2 then
-    rate = util.clamp(rate + z, 1, 100)
-    crow.input[1].mode("stream", 1/rate)
+    rate = util.clamp(rate + z*.01, .01, 2)
+    crow.input[1].mode("stream", 1/(rate*127))
+    crow.input[2].mode("stream", 1/(rate*127))
   elseif n==3 then
-    offset = util.clamp(offset - z, 0, 50)
+    if enc3Mode == 'offset' then
+      offset = util.clamp(offset - z, 0, 50)
+    elseif enc3Mode == 'yrange' then
+      yrange = util.clamp(yrange + z*.5, 1, 8)
+    end
   end
 end 
 
 function key(n, z)
-  if n==2 and z==1 and xaxis then
-    xaxis = false
-  elseif n==2 and z==1 and not axis then
-    xaxis = true
-  elseif n==3 and z==1 and display2 then
-    display2 = false
-  elseif n==3 and z==1 and not display2 then
-    display2 = true
+  if n==1 and z==1 then
+    enc3Mode = 'offset'
+  elseif n==1 and z==-1 then
+    enc3Mode = 'yrange'
+  elseif n==2 and z==1 then
+    if xaxis then
+      xaxis = false
+    else
+      xaxis = true
+    end
+  elseif n==3 and z==1 then
+    if display2 then
+      display2 = false
+    else
+      display2 = true
+    end
   end
 end
